998c7c65c003a7f0ef4182342fdb265d217aeb5f
toney <197585312@qq.com>
2017-01-16 00:19:46 +0800

    aaa

--- baseLibs/src/main/java/com/unkonw/testapp/training/ScrollLayout.java
+++ baseLibs/src/main/java/com/unkonw/testapp/training/ScrollLayout.java
@@ -8,6 +8,8 @@ import android.view.ViewConfiguration;
 import android.view.ViewGroup;
 import android.widget.Scroller;
 
+import com.orhanobut.logger.Logger;
+
 /**
  * Created by Administrator on 2017/1/14 0014.
  */
@@ -18,6 +20,9 @@ public class ScrollLayout extends ViewGroup {
     private Scroller scroller;
     private int leftBorder;
     private int rightBorder;
+    private float rawX;
+    private float lastX;
+    private float rawX2;
 
     public ScrollLayout(Context context) {
         super(context);
@@ -48,17 +53,35 @@ public class ScrollLayout extends ViewGroup {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         int childCount = getChildCount();
         for (int i = 0; i < childCount; i++) {
-           measureChild( getChildAt(0),widthMeasureSpec,heightMeasureSpec);
+           measureChild( getChildAt(i),widthMeasureSpec,heightMeasureSpec);
         }
     }
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
+        int action = ev.getAction() & MotionEvent.ACTION_MASK;
+       switch (action) {
+           case MotionEvent.ACTION_DOWN:
+               rawX = ev.getRawX();
+               lastX=rawX;
+               break;
+           case MotionEvent.ACTION_MOVE:
+               rawX2 = ev.getRawX();
+               float v = rawX2 - rawX;
+               lastX=rawX2;
+               if(v>scaledTouchSlop) {
+                   Logger.d("v>"+scaledTouchSlop);
+                   return true;
+
+               }
+               break;
+       }
         return super.onInterceptTouchEvent(ev);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+
         return super.onTouchEvent(event);
     }
 }
