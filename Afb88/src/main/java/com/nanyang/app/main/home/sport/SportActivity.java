36a75a43ba57f2386376dbe87cd0c52568d3ced5
jc <yangjiechao945@qq.com.com>
2017-02-24 17:18:58 +0800

    mvp

--- Afb88/src/main/java/com/nanyang/app/main/home/sport/SportActivity.java
+++ Afb88/src/main/java/com/nanyang/app/main/home/sport/SportActivity.java
@@ -1,6 +1,8 @@
 package com.nanyang.app.main.home.sport;
 
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
@@ -9,10 +11,17 @@ import android.widget.TextView;
 
 import com.nanyang.app.AppConstant;
 import com.nanyang.app.BaseToolbarActivity;
+import com.nanyang.app.MenuItemInfo;
 import com.nanyang.app.R;
 import com.nanyang.app.main.home.sport.basketball.BasketballFragment;
+import com.nanyang.app.main.home.sport.thaiThousand.ThaiThousandFragment;
+import com.unkonw.testapp.libs.adapter.BaseRecyclerAdapter;
+import com.unkonw.testapp.libs.adapter.MyRecyclerViewHolder;
+import com.unkonw.testapp.libs.widget.BasePopupWindow;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -23,6 +32,7 @@ public class SportActivity extends BaseToolbarActivity<Presenter> {
     BaseSportFragment footballFragment = new FootballFragment();
     BaseSportFragment basketballFragment = new BasketballFragment();
     BaseSportFragment volleyballFragment = new VolleyballFragment();
+    BaseSportFragment taiFragment = new ThaiThousandFragment();
 
     @Bind(R.id.iv_add)
     ImageView ivAdd;
@@ -86,6 +96,7 @@ public class SportActivity extends BaseToolbarActivity<Presenter> {
         mapFragment.put(getString(R.string.Football), footballFragment);
         mapFragment.put(getString(R.string.Basketball), basketballFragment);
         mapFragment.put(getString(R.string.Volleyball), volleyballFragment);
+        mapFragment.put(getString(R.string.Thai_word), taiFragment);
 
 
     }
@@ -100,7 +111,7 @@ public class SportActivity extends BaseToolbarActivity<Presenter> {
         }
     }
 
-    @OnClick({R.id.tv_refresh, R.id.tv_collection, R.id.tv_menu, R.id.tv_mix})
+    @OnClick({R.id.tv_refresh, R.id.tv_collection, R.id.tv_menu, R.id.tv_mix, R.id.iv_add})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.tv_refresh:
@@ -116,7 +127,50 @@ public class SportActivity extends BaseToolbarActivity<Presenter> {
                 if (currentFragment.mixParlayCLick(tvMix))
                     tvCollection.setCompoundDrawablesWithIntrinsicBounds(0, R.mipmap.sport_star_black, 0, 0);
                 break;
+            case R.id.iv_add:
+                createPopupWindow(new BasePopupWindow(mContext, view, LinearLayout.LayoutParams.MATCH_PARENT, 400) {
+                    @Override
+                    protected int onSetLayoutRes() {
+                        return R.layout.popupwindow_choice;
+                    }
 
+                    @Override
+                    protected void initView(View view) {
+                        super.initView(view);
+                        RecyclerView rv = (RecyclerView) view.findViewById(R.id.rv_list);
+                        rv.setLayoutManager(new GridLayoutManager(mContext, 3));
+                        List<MenuItemInfo> list = new ArrayList<>();
+                        list.add(new MenuItemInfo(0, getString(R.string.Football)));
+                        list.add(new MenuItemInfo(0, getString(R.string.Basketball)));
+                        list.add(new MenuItemInfo(0, getString(R.string.Volleyball)));
+                        list.add(new MenuItemInfo(0, getString(R.string.Thai_word)));
+                        BaseRecyclerAdapter<MenuItemInfo> baseRecyclerAdapter = new BaseRecyclerAdapter<MenuItemInfo>(mContext, list, R.layout.text_base) {
+                            @Override
+                            public void convert(MyRecyclerViewHolder holder, int position, MenuItemInfo item) {
+                                TextView tv = holder.getView(R.id.item_text_tv);
+                                tv.setText(item.getText());
+                                tv.setBackgroundResource(R.color.white);
+                                tv.setTextColor(getResources().getColor(R.color.black_grey));
+                            }
+
+
+                        };
+
+                        rv.setAdapter(baseRecyclerAdapter);
+                        baseRecyclerAdapter.setOnItemClickListener(new BaseRecyclerAdapter.OnItemClickListener<MenuItemInfo>() {
+                            @Override
+                            public void onItemClick(View view, MenuItemInfo item, int position) {
+                                selectFragmentTag(item.getText());
+
+                                closePopupWindow();
+                            }
+                        });
+                    }
+                });
+                popWindow.showPopupDownWindow();
+                break;
         }
     }
+
+
 }
