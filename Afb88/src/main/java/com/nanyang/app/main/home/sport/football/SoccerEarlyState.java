22aa1ae1f2b57f58f052908a349823d740870de7
jc <yangjiechao945@qq.com.com>
2017-03-25 10:38:25 +0800

    Merge branch 'master' of https://github.com/q197585312/testApp
    # Conflicts:
    #	Afb88/src/main/java/com/nanyang/app/ApiService.java
    #	Afb88/src/main/java/com/nanyang/app/main/home/sport/SportActivity.java
    #	Afb88/src/main/res/values/strings.xml

--- Afb88/src/main/java/com/nanyang/app/main/home/sport/football/SoccerEarlyState.java
+++ Afb88/src/main/java/com/nanyang/app/main/home/sport/football/SoccerEarlyState.java
@@ -1,13 +1,25 @@
 package com.nanyang.app.main.home.sport.football;
 
+import android.support.annotation.NonNull;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.nanyang.app.AppConstant;
 import com.nanyang.app.MenuItemInfo;
 import com.nanyang.app.R;
 import com.nanyang.app.main.home.sport.main.SportContract;
 import com.unkonw.testapp.libs.adapter.BaseRecyclerAdapter;
+import com.unkonw.testapp.libs.adapter.MyRecyclerViewHolder;
+import com.unkonw.testapp.libs.utils.TimeUtils;
+import com.unkonw.testapp.libs.widget.BasePopupWindow;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -52,13 +64,14 @@ public class SoccerEarlyState extends SoccerCommonState {
 
     @Override
     public MenuItemInfo getStateType() {
-        return new MenuItemInfo<String>(0,getBaseView().getContextActivity().getString(R.string.Early),"Early",getBaseView().getContextActivity().getString(R.string.football));
+        return new MenuItemInfo<String>(0, getBaseView().getContextActivity().getString(R.string.Early), "Early", getBaseView().getContextActivity().getString(R.string.football));
     }
 
     @Override
     protected SoccerCommonAdapterHelper onSetCommonAdapterHelper() {
         return new SoccerEarlyAdapterHelper(getBaseView().getContextActivity());
     }
+
     @Override
     protected void bindMenuAdapter(BaseRecyclerAdapter<MenuItemInfo> baseRecyclerAdapter, List<MenuItemInfo> types) {
         types.add(new MenuItemInfo(R.mipmap.menu_date_white, getBaseView().getContextActivity().getString(R.string.date), "Date"));
@@ -68,35 +81,13 @@ public class SoccerEarlyState extends SoccerCommonState {
     @Override
     protected void popMenuItemClick(View view, MenuItemInfo item) {
         super.popMenuItemClick(view, item);
-        if(item.getType().equals("Date")){
+        if (item.getType().equals("Date")) {
 
         }
     }
-/*    private void showDateChoicePrompt() {
-        BasePopupWindow popChoice = new BasePopupWindow (getBaseView().getContextActivity(), new View(getBaseView().getContextActivity()), 800, LinearLayout.LayoutParams.WRAP_CONTENT) {
 
-
-            @Override
-            protected void popItemCLick(MenuItemInfo item, int position) {
-                closePopupWindow();
-                getEarlyDayData(item);
-            }
-
-            @Override
-            protected void convertItem(ViewHolder helper, MenuItemInfo item, int position) {
-                helper.setText(R.id.item_text_tv, item.getTitle());
-
-            }
-
-            @Override
-            protected int getItemLayoutRes() {
-                return R.layout.item_text_base;
-            }
-
-            @Override
-            protected int getListViewId() {
-                return R.id.popup_choice_lv;
-            }
+    private void showDateChoicePrompt() {
+        BasePopupWindow popChoice = new BasePopupWindow(getBaseView().getContextActivity(), new View(getBaseView().getContextActivity()), 800, LinearLayout.LayoutParams.WRAP_CONTENT) {
 
             @Override
             protected int onSetLayoutRes() {
@@ -106,37 +97,58 @@ public class SoccerEarlyState extends SoccerCommonState {
             @Override
             protected void initView(View view) {
                 super.initView(view);
-                ListView lv = (ListView) view.findViewById(R.id.popup_choice_lv);
-                View header = LayoutInflater.from(getBaseView().getContextActivity()).inflate(R.layout.item_text, null);
 
+
+                RecyclerView lv = (RecyclerView) view.findViewById(R.id.base_rv);
+                lv.setLayoutManager(new LinearLayoutManager(getBaseView().getContextActivity()));
+                BaseRecyclerAdapter<MenuItemInfo> adapter = getAdapter();
+                adapter.setOnItemClickListener(new BaseRecyclerAdapter.OnItemClickListener<MenuItemInfo>() {
+                    @Override
+                    public void onItemClick(View view, MenuItemInfo item, int position) {
+                        closePopupWindow();
+//                        getEarlyDayData(item);
+                    }
+                });
+                lv.setAdapter(adapter);
+
+            }
+
+            @NonNull
+            private BaseRecyclerAdapter<MenuItemInfo> getAdapter() {
+                String h12 = TimeUtils.dateFormat(new Date(), "yyyy-MM-dd") + " 12:00:00";
+                String now = TimeUtils.dateFormat(new Date(), "yyyy-MM-dd HH:mm:ss");
+                long dif = TimeUtils.diffTime(now, h12, "yyyy-MM-dd HH:mm:ss");
+                Date firstDate = new Date();
+                if (dif < 0)
+                    firstDate = TimeUtils.getAddDayDate(firstDate, -1);
+                String d1 = TimeUtils.dateFormat(TimeUtils.getAddDayDate(firstDate, 1), "yyyy-MM-dd");
+                String d2 = TimeUtils.dateFormat(TimeUtils.getAddDayDate(firstDate, 2), "yyyy-MM-dd");
+                String d3 = TimeUtils.dateFormat(TimeUtils.getAddDayDate(firstDate, 3), "yyyy-MM-dd");
+                String d4 = TimeUtils.dateFormat(TimeUtils.getAddDayDate(firstDate, 4), "yyyy-MM-dd");
+                String d5 = TimeUtils.dateFormat(TimeUtils.getAddDayDate(firstDate, 5), "yyyy-MM-dd");
+                String dv = TimeUtils.dateFormat(TimeUtils.getAddDayDate(firstDate, 6), "yyyy-MM-dd");
+                String d6 = getBaseView().getContextActivity().getString(R.string.six_day);
+                MenuItemInfo item0 = new MenuItemInfo(0, getBaseView().getContextActivity().getString(R.string.all), "");
+                MenuItemInfo item1 = new MenuItemInfo(0, d1, d1);
+                MenuItemInfo item2 = new MenuItemInfo(0, d2, d2);
+                MenuItemInfo item3 = new MenuItemInfo(0, d3, d3);
+                MenuItemInfo item4 = new MenuItemInfo(0, d4, d4);
+                MenuItemInfo item5 = new MenuItemInfo(0, d5, d5);
+                MenuItemInfo item6 = new MenuItemInfo(1, d6, dv);
+                return new BaseRecyclerAdapter<MenuItemInfo>(getBaseView().getContextActivity(),new ArrayList<>(Arrays.asList(item0, item1, item2, item3, item4, item5, item6)), R.layout.text_base_item) {
+                    @Override
+                    public void convert(MyRecyclerViewHolder holder, int position, MenuItemInfo item) {
+                        TextView view1 = holder.getView(R.id.item_text_tv);
+                        view1.setBackgroundResource(R.drawable.rectangle_button_green);
+                        view1.setText(item.getText());
+                        view1.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
+                    }
+                };
             }
         };
 
 
-        getBaseView().onPopupWindowCreated();
+        getBaseView().onPopupWindowCreated(popChoice, Gravity.CENTER);
 
-        String h12 = TimeUtils.dateFormat(new Date(), "yyyy-MM-dd") + " 12:00:00";
-        String now = TimeUtils.dateFormat(new Date(), "yyyy-MM-dd HH:mm:ss");
-        long dif = TimeUtils.diffTime(now, h12, "yyyy-MM-dd HH:mm:ss");
-        Date firstDate = new Date();
-        if (dif < 0)
-            firstDate = TimeUtils.getAddDayDate(firstDate, -1);
-        String d1 = TimeUtils.dateFormat(TimeUtils.getAddDayDate(firstDate, 1), "yyyy-MM-dd");
-        String d2 = TimeUtils.dateFormat(TimeUtils.getAddDayDate(firstDate, 2), "yyyy-MM-dd");
-        String d3 = TimeUtils.dateFormat(TimeUtils.getAddDayDate(firstDate, 3), "yyyy-MM-dd");
-        String d4 = TimeUtils.dateFormat(TimeUtils.getAddDayDate(firstDate, 4), "yyyy-MM-dd");
-        String d5 = TimeUtils.dateFormat(TimeUtils.getAddDayDate(firstDate, 5), "yyyy-MM-dd");
-        String dv = TimeUtils.dateFormat(TimeUtils.getAddDayDate(firstDate, 6), "yyyy-MM-dd");
-        String d6 = getString(R.string.six_day);
-        MenuItemInfo item0 = new MenuItemInfo(0, getString(R.string.all), "");
-        MenuItemInfo item1 = new MenuItemInfo(0, d1, d1);
-        MenuItemInfo item2 = new MenuItemInfo(0, d2, d2);
-        MenuItemInfo item3 = new MenuItemInfo(0, d3, d3);
-        MenuItemInfo item4 = new MenuItemInfo(0, d4, d4);
-        MenuItemInfo item5 = new MenuItemInfo(0, d5, d5);
-        MenuItemInfo item6 = new MenuItemInfo(1, d6, dv);
-        popChoice.setData(new ArrayList<MenuItemInfo>(Arrays.asList(item0, item1, item2, item3, item4, item5, item6)));
-        popChoice.showPopupCenterWindow();
-
-    }*/
+    }
 }
