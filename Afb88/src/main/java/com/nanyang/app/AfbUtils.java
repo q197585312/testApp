5aa39f0818da58d18a27ef367f5a69ae2391b6f0
shangpeisheng <15123048687@163.com>
2017-03-31 22:32:19 +0800

    aaa

--- Afb88/src/main/java/com/nanyang/app/AfbUtils.java
+++ Afb88/src/main/java/com/nanyang/app/AfbUtils.java
@@ -235,7 +235,7 @@ public class AfbUtils {
         while (baos.toByteArray().length / 1024 > 100) {    // 循环判断如果压缩后图片是否大于100kb,大于继续压缩
             baos.reset();// 重置baos即清空baos
             options -= 10;// 每次都减少10
-            image.compress(Bitmap.CompressFormat.PNG, options, baos);// 这里压缩options%，把压缩后的数据存放到baos中
+            image.compress(Bitmap.CompressFormat.JPEG, options, baos);// 这里压缩options%，把压缩后的数据存放到baos中
 
         }
         ByteArrayInputStream isBm = new ByteArrayInputStream(baos.toByteArray());// 把压缩后的数据baos存放到ByteArrayInputStream中
