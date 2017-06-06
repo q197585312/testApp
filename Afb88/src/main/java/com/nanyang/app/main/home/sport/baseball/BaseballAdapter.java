c72e4494d47b8b080c285767e43594b44f7e39e7
jc <yangjiechao945@qq.com.com>
2017-06-06 18:00:02 +0800

    Merge branch 'master' of https://github.com/q197585312/testApp
    # Conflicts:
    #	Afb88/src/main/java/com/nanyang/app/ApiService.java
    #	Afb88/src/main/java/com/nanyang/app/main/home/sport/SportActivity.java
    #	Afb88/src/main/res/values/strings.xml

---
+++ Afb88/src/main/java/com/nanyang/app/main/home/sport/baseball/BaseballAdapter.java
@@ -0,0 +1,8 @@
+package com.nanyang.app.main.home.sport.baseball;
+
+/**
+ * Created by Administrator on 2017/6/6.
+ */
+
+public class BaseballAdapter extends  {
+}
--- Afb88/src/main/java/com/nanyang/app/main/home/sport/baseball/BaseballState.java
+++ Afb88/src/main/java/com/nanyang/app/main/home/sport/baseball/BaseballState.java
@@ -8,6 +8,8 @@ import com.nanyang.app.main.home.sport.main.SportContract;
 import com.nanyang.app.main.home.sport.tennis.TennisState;
 import com.nanyang.app.main.home.sportInterface.IAdapterHelper;
 import com.nanyang.app.main.home.sportInterface.IBetHelper;
+import com.unkonw.testapp.libs.adapter.MyRecyclerViewHolder;
+import com.unkonw.testapp.training.ScrollLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,7 +30,19 @@ public abstract class BaseballState extends TennisState{
 
     @Override
     public IAdapterHelper<BasketballMixInfo> onSetAdapterHelper() {
-        return new OtherAdapterHelper(getBaseView().getContextActivity());
+        return new OtherAdapterHelper(getBaseView().getContextActivity()){
+            @Override
+            public void onConvert(MyRecyclerViewHolder helper, int position, BasketballMixInfo item) {
+                super.onConvert(helper, position, item);
+                ScrollLayout sl = helper.getView(R.id.module_center_sl);
+                scrollChild(sl.getChildAt(1),true, item, item.getIsHomeGive_FH(), item.getHasHdp_FH(), item.getHdp_FH(), item.getHasOU_FH(), item.getOU_FH(), "0", "0", item.getUnderOdds_FH(), item.getOverOdds_FH(), item.getHomeHdpOdds_FH(), item.getAwayHdpOdds_FH());
+
+                scrollChild(sl.getChildAt(1), false, item, item.getIsHomeGive(), item.getHasX12(), "", item.getHasOE(), "", item.getIsX12New(), "", "", "", item.getX12_1Odds(), item.getX12_2Odds(), "1", "2", "", ""
+                        , true, false, true, item.getHasOE(), item.getIsOENew(), item.getOddOdds(), item.getEvenOdds(), "odd", "even");
+                getBaseRecyclerAdapter().getItem(position).setIsX12New("0");
+                getBaseRecyclerAdapter().getItem(position).setIsOENew("0");
+            }
+        };
     }
     @Override
     protected List<MenuItemInfo> getTypes() {
