5d156f688e14b72f3997fd3624a1d5dab247862c
jc <yangjiechao945@qq.com.com>
2017-07-19 17:44:28 +0800

    Merge branch 'master' of https://github.com/q197585312/testApp
    # Conflicts:
    #	Afb88/src/main/java/com/nanyang/app/ApiService.java
    #	Afb88/src/main/java/com/nanyang/app/main/home/sport/SportActivity.java
    #	Afb88/src/main/res/values/strings.xml

--- dongnao/src/main/java/com/example/dongnao/MainActivity.java
+++ dongnao/src/main/java/com/example/dongnao/MainActivity.java
@@ -10,10 +10,7 @@ import android.view.View;
 /**
  * Created by Administrator on 2017/7/18.
  */
-/**先触发 onTouch再触发 onClick
- * 如果 onTouch 返回true 则只触发onTouch，只有返回false 才会传递 onCLick事件
- * 依据 view的时间分发
- * */
+
 public class MainActivity extends AppCompatActivity implements View.OnTouchListener, View.OnClickListener {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -27,10 +24,24 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         viewById.setOnClickListener(this);
 
     }
-
+    /**先触发 onTouch再触发 onClick
+     * 如果 onTouch 返回true 则只触发onTouch，只有返回false 才会传递 onCLick事件
+     * 依据 view事件分发
+     * */
+    /**view 事件分发
+     * 1、dispatchTouchEvent（）分发
+     * 2、 onTouchEvent view的回调  设置onTouchListener  后进入----》  方法 onTouch（）
+     * r如果设置了O你TouchListener里面的onTouch返回了 true  则里面子view的touch 不执行 且自身的 onTouchEvent不执行
+     * dispatchTouchEvent--->onTouchListener --->onTouch----返回false----> 执行自身onTouchEvent
+     * 如果view为disEnable 则 onTouchListener 不会执行----> 自身onTouchEvent
+     * 3、 onClick----》OnClickListener
+     * onClick 在 自身的onTouchEvent方法里面
+     *
+     * */
     @Override
     public boolean onTouch(View view, MotionEvent motionEvent) {
         Log.i("test","Touch;action---"+motionEvent.getAction()+"----view:"+view);
+        View
         return true;
     }
 
