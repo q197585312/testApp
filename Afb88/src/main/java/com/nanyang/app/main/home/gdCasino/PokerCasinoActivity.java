7cc244fb60ab49fecf602f9d644d869f67f24ba1
shangpeisheng <15123048687@163.com>
2017-08-31 09:33:52 +0800

    aaa

--- Afb88/src/main/java/com/nanyang/app/main/home/gdCasino/PokerCasinoActivity.java
+++ Afb88/src/main/java/com/nanyang/app/main/home/gdCasino/PokerCasinoActivity.java
@@ -243,6 +243,7 @@ public class PokerCasinoActivity extends BaseToolbarActivity<LanguagePresenter> 
             bundle.putString("k", data);
             bundle.putString("us", getApp().getUser().getUserName());
             bundle.putString("lang",AfbUtils.getLanguage(mContext));
+            bundle.putString("appName",getString(R.string.app_name));
             try {
 //                AfbUtils.appJump(mContext, "gaming178.com.baccaratgame", "gaming178.com.casinogame.Activity.WelcomeActivity", bundle);
                 Intent intent = new Intent(Intent.ACTION_MAIN);
