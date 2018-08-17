dba2c2b19cf51c3802660e3f4d7b742aba6ca8e2
shangpeisheng <shang.1906.dx>
2018-08-17 09:42:07 +0800

    在家修改代码

--- Afb88/src/main/java/com/nanyang/app/main/home/sport/myanmarOdds/MyanmarRunningAdapterHelper.java
+++ Afb88/src/main/java/com/nanyang/app/main/home/sport/myanmarOdds/MyanmarRunningAdapterHelper.java
@@ -22,23 +22,34 @@ public class MyanmarRunningAdapterHelper extends MyanmarAdapterHelper {
     public void onConvert(MyRecyclerViewHolder helper, int position, final MyanmarInfo item) {
         super.onConvert(helper, position, item);
         TextView dateTv=helper.getView(R.id.module_match_date_tv);
+        TextView dateTv1=helper.getView(R.id.module_match_date_tv1);
         TextView liveTv=helper.getView(R.id.module_match_live_iv);
+        TextView liveTv1=helper.getView(R.id.module_match_live_iv1);
         TextView timeTv=helper.getView(R.id.module_match_time_tv);
+        TextView timeTv1=helper.getView(R.id.module_match_time_tv1);
         dateTv.setTextAppearance(context, R.style.text_bold);
+        dateTv1.setTextAppearance(context, R.style.text_bold);
         View viewRight = helper.getView(R.id.module_right_mark_tv);
+        View viewRight1 = helper.getView(R.id.fl_right);
         viewRight.setVisibility(View.GONE);
+        viewRight1.setVisibility(View.GONE);
         dateTv.setPadding(0, 0, 10, 0);
+        dateTv1.setPadding(0, 0, 10, 0);
         liveTv.setVisibility(View.GONE);
+        liveTv1.setVisibility(View.GONE);
         if (item.getRunHomeScore() != null && item.getRunAwayScore() != null && !item.getRunAwayScore().equals("") && !item.getRunHomeScore().equals("")) {
             String sHome = item.getRunHomeScore();
             String sAway = item.getRunAwayScore();
             dateTv.setText(sHome + "-" + sAway);
+            dateTv1.setText(sHome + "-" + sAway);
 
         } else {
             dateTv.setText("");
+            dateTv1.setText("");
         }
         if (item.getLive().contains("HT")) {
             timeTv.setText("HT");
+            timeTv1.setText("HT");
         } else {
             int min;
             int start;
@@ -47,6 +58,7 @@ public class MyanmarRunningAdapterHelper extends MyanmarAdapterHelper {
                 switch (item.getStatus()) {
                     case "0":
                         timeTv.setText(context.getString(R.string.not_started));
+                        timeTv1.setText(context.getString(R.string.not_started));
                         break;
                     case "2":
                         min = Integer.valueOf(item.getCurMinute());
@@ -54,16 +66,20 @@ public class MyanmarRunningAdapterHelper extends MyanmarAdapterHelper {
                         min = min + start;
                         if (min < 130 && min > 0) {
                             timeTv.setText(min + context.getString(R.string.min));
+                            timeTv1.setText(min + context.getString(R.string.min));
                         } else {
                             timeTv.setText("");
+                            timeTv1.setText("");
                         }
                         break;
                     default:
                         min = Integer.valueOf(item.getCurMinute());
                         if (min < 130 && min > 0) {
                             timeTv.setText(min + context.getString(R.string.min));
+                            timeTv1.setText(min + context.getString(R.string.min));
                         } else {
                             timeTv.setText("");
+                            timeTv1.setText("");
                         }
                         break;
                 }
@@ -72,10 +88,13 @@ public class MyanmarRunningAdapterHelper extends MyanmarAdapterHelper {
             } catch (Exception e) {
                 e.printStackTrace();
                 timeTv.setText("");
+                timeTv1.setText("");
             }
         }
         dateTv.setTextColor(context.getResources().getColor(R.color.red_title));
+        dateTv1.setTextColor(context.getResources().getColor(R.color.red_title));
         timeTv.setTextColor(context.getResources().getColor(R.color.red_title));
+        timeTv1.setTextColor(context.getResources().getColor(R.color.red_title));
     }
 
 
