4040655bc68a9db7a4bd5fe216428fdcd85027d1
shangpeisheng <shang.1906.dx>
2020-06-22 14:30:13 +0800

    测试提交

--- app/src/main/java/gaming178/com/casinogame/Activity/BaccaratActivity.java
+++ app/src/main/java/gaming178/com/casinogame/Activity/BaccaratActivity.java
@@ -3439,6 +3439,7 @@ public class BaccaratActivity extends BaseActivity implements UseLandscape {
             }
         }
 //        path = "rtmp://202.36.58.169/live/M01";
+        //测试提交
         videoHelper.setPlayUrl(path);
     }
 
